9df7734c8240b6e83bfccaf1947296c2385b5fd0
yangliu <yangliulnn@163.com>
2021-12-01 17:55:31 +0800

    0.2.1

--- contracts/interfaces/IFactory.sol
+++ contracts/interfaces/IFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 
 interface IFactory {
     function getExchange(address token) external view returns (address exchange);
